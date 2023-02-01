d3e9debe0721ffca365323e17c9e6e4b4374016a
milk717 <robolindasoo@gmail.com>
2023-02-01 23:00:32 +0900

    feat: 쿼리문으로 오늘의 리마인더, 활성화된 리마인더 조회

--- src/api/reminder/reminder.ctrl.js
+++ src/api/reminder/reminder.ctrl.js
@@ -1,24 +1,30 @@
 //
 import Reminder from '../../models/schema/reminder.js';
-
 import {ProcessingPostDto} from "../../utils/ProcessingPostDto.js";
 import dayjs from "dayjs";
 
 export const getReminderList = async ctx => {
     try {
+        let query = ctx.request.query;
         let reminderList = await Reminder.find().exec();
-        ctx.body = reminderList.filter(v=>dayjs(v.startDate).add(-9,'hour').isSame(dayjs(),'day'));
+        if(query.filter === 'today'){
+            reminderList = reminderList.filter(v => dayjs(v.startDate).add(-9, 'hour').isSame(dayjs(), 'day'))
+        }
+        else if(query.filter === 'active'){
+            reminderList = reminderList.filter(v => !(dayjs(v.endDate).add(-9, 'hour').isBefore(dayjs())))
+        }
+        ctx.body = reminderList;
     } catch (e) {
         ctx.throw(500, e)
     }
 }
 
 export const postReminder = async ctx => {
-    const reminderPostDto                 = ctx.request.body;
+    const reminderPostDto = ctx.request.body;
     const reminderRepetitionList = new ProcessingPostDto(reminderPostDto).getReminderRepetitionList();
-    const reminderList = reminderRepetitionList.map((reminder)=>new Reminder(reminder));
+    const reminderList = reminderRepetitionList.map((reminder) => new Reminder(reminder));
     try {
-        console.log('reminder post result\n',reminderList)
+        console.log('reminder post result\n', reminderList)
         for (const reminder of reminderList) {
             await reminder.save();
         }
