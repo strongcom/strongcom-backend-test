57909b80547e4d53d0b9f7db2eac60c948223828
milk717 <robolindasoo@gmail.com>
2023-04-23 22:32:22 +0900

    feat: 유저 아이디 전송용

--- src/api/push/index.js
+++ src/api/push/index.js
@@ -4,5 +4,6 @@ import * as pushCtrl from "./push.ctrl.js";
 const push = new Router();
 
 push.get('/test', pushCtrl.test);
+push.post('/userId', pushCtrl.detectedUser);
 
 export default push;
--- src/api/push/push.ctrl.js
+++ src/api/push/push.ctrl.js
@@ -3,3 +3,12 @@ import notificationController from "../../controller/notificationController.js";
 export const test = async ctx => {
     ctx = {...await notificationController(ctx)}
 }
+
+export const detectedUser = async ctx => {
+    const {userId} = ctx.request.body;
+    try{
+        ctx.body = userId;
+    }catch (e) {
+        ctx.throw(500, e);
+    }
+}
