1ec7a63619e0ddce4fcf1abad6c0d5ed840d2f47
milk717 <robolindasoo@gmail.com>
2023-05-10 13:50:50 +0900

    feat: 알람 울리고 나면 삭제됨

--- src/controller/notificationController.js
+++ src/controller/notificationController.js
@@ -10,6 +10,17 @@ export default function notificationController() {
             credential: admin.credential.cert(firebaseKey),
         });
     }
+    const pushNotice = async ({message}) => {
+        await admin
+            .messaging()
+            .send(message)
+            .then((response) => {
+                console.log('success', response);
+            })
+            .catch((error) => {
+                console.log('success', error);
+            });
+    }
     const testPush = async (ctx) => {
         let deviceToken = 'cdjNdVk_Rme3Z4E8868dmo:APA91bGIvnL4HogW7-p3_gtrJ1sYeVIR2XzOEJGHXOhL563ODarBzAJLjZCSecYc4H8our__ItYrEdXW--yhKi279C1Ir7EAx6lRE-Ivv25N6ooIco_B4sHjbE0fXSqAVdSklX13a8mS';
 
@@ -20,28 +31,17 @@ export default function notificationController() {
             token: deviceToken
         }
 
-        await admin
-            .messaging()
-            .send(message)
-            .then((response) => {
-                console.log('success', response);
-                ctx.status = 204;
-            })
-            .catch((error) => {
-                console.log('success', error);
-                ctx.status = 404;
-                ctx.body = error;
-            });
+        await pushNotice(message);
 
         return ctx;
     }
 
-    const pushNotifications = async (ctx) => {
+    const findTodayReminders = async ({username}) => {
         const date = dayjs();
         const today = date.format('YYYY-MM-DD');
         const now = date.format('hh:mm:ss');
-        const user = await User.findOne({username: ctx.request.body.username});
-        const todayReminders = await Reminder
+        const user = await User.findOne({username: username});
+        return Reminder
             .find({
                 $and: [
                     {"userInfo._id": user._id},
@@ -50,6 +50,33 @@ export default function notificationController() {
                     {endTime: {$gte: now}},
                 ]
             });
+    };
+
+    const removeNoticesDate = async ({username}) => {
+        const date = dayjs();
+        const today = date.format('YYYY-MM-DD');
+        const now = date.format('hh:mm:ss');
+        const user = await User.findOne({username: username});
+        await Reminder.updateMany({
+                $and: [
+                    {"userInfo._id": user._id},
+                    {notices: today},
+                    {startTime: {$lte: now}},
+                    {endTime: {$gte: now}},
+                ]
+            }, {
+                $pull: {notices: today}
+            },
+            {multi: true});
+    };
+
+    const removeCompletedReminder = async () => {
+        await Reminder.deleteMany({notices: {$size: 0}},);
+    }
+
+    const pushNotifications = async (ctx) => {
+        const todayReminders = await findTodayReminders({username: ctx.request.body.username})
+
         for (const reminder of todayReminders) {
             let message = {
                 notification: {
@@ -57,16 +84,11 @@ export default function notificationController() {
                 },
                 token: user.targetToken
             };
-            await admin
-                .messaging()
-                .send(message)
-                .then((response) => {
-                    console.log('success', response);
-                })
-                .catch((error) => {
-                    console.log('success', error);
-                });
+            await pushNotice(message);
         }
+        await removeNoticesDate({username: ctx.request.body.username}).then(
+            await removeCompletedReminder()
+        )
     }
 
     return {
